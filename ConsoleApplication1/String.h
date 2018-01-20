b608a15e3f68f4318d357211897e2b81073aa6eb
Kirill Grabuzdov <sumpory@gmail.com>
2018-01-20 11:21:47 +0300

    Заголовок

--- ConsoleApplication1/String.h
+++ ConsoleApplication1/String.h
@@ -4,23 +4,23 @@
 #include "stdafx.h"
 
 
-class ourstring
+class string
 {
 public:
 	int size = 256; // макс размер строки 
 	char *str = new char[size]; // строка 
 	int len = 0; // длина строки 
-	ourstring() {} // пустой конструктор 
-	~ourstring() { // деконструктор 
+	string() {} // пустой конструктор 
+	~string() { // деконструктор 
 		delete[] str;
 	}
-	ourstring(const char*a); // конструктор ourstring a = "string"; 
-	ourstring(std::string a);
+	string(const char*a); // конструктор ourstring a = "string"; 
+	string(std::string a);
 	void add(const char* a); // добавляем в конец строки символы " " 
 	void clear(); // очистка строки 
 	void cut(int l, int r); //очистить с l по r ?? 
 };
-void ourstring::cut(int l, int r) //позиция, количество 
+void string::cut(int l, int r) //позиция, количество 
 {
 	if (l >= 0 && l + r < size) {
 		for (int i = l; i + r < len; i++) {
@@ -34,7 +34,7 @@ void ourstring::cut(int l, int r) //позиция, количество
 	else
 		std::cout << "Неверные введённые данные (выход за пределы строки)";
 }
-ourstring::ourstring(const char*a)
+string::string(const char*a)
 {
 	while (a[len])
 	{
@@ -42,14 +42,14 @@ ourstring::ourstring(const char*a)
 		len++;
 	}
 }
-ourstring::ourstring(std::string a)
+string::string(std::string a)
 {
 	len = a.length();
 	for (int i = 0; i < len; i++) {
 		str[i] = a[i];
 	}
 }
-void ourstring::add(const char* a)
+void string::add(const char* a)
 {
 	int i = 0;
 	while (a[i])
@@ -59,7 +59,7 @@ void ourstring::add(const char* a)
 	}
 	len += i;
 }
-void ourstring::clear()
+void string::clear()
 {
 	for (int i = 0; i < len; i++) {
 		str[i] = 0;
